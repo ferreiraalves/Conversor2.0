9e0e3f564b19e11ebfd97a8ee71d7349e1610148
Lucas Alves <ferreira.alves@live.com>
2018-03-22 15:32:14 -0300

    Integração com AWS e Zencoder

--- src/main/java/S3Servicelet.java
+++ src/main/java/S3Servicelet.java
@@ -1,66 +1,199 @@
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
+import java.io.StringReader;
+import java.io.UnsupportedEncodingException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Enumeration;
+import java.util.List;
 
+import javax.net.ssl.HttpsURLConnection;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.client.methods.HttpPut;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.util.EntityUtils;
+import org.json.JSONArray;
+import org.json.JSONObject;
+
 //
 import com.amazonaws.AmazonClientException;
 import com.amazonaws.AmazonServiceException;
+import com.amazonaws.HttpMethod;
 import com.amazonaws.auth.profile.ProfileCredentialsProvider;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3Client;
+import com.amazonaws.services.s3.model.GeneratePresignedUrlRequest;
 import com.amazonaws.services.s3.model.PutObjectRequest;
-import com.amazonaws.services.s3.model.PutObjectResult;
+
+
 //
+
+
 
 @WebServlet("/s3")
 public class S3Servicelet extends HttpServlet {
-	private String bucketName = "bucketconversorsamba";
-	private String keyName = "marioza.jpg";
-	private String uploadFileName = "C:\\Users\\ferre\\Pictures\\21740635_1124692240997517_6652591090726829763_n.jpg";
+	private String  bucketName    = "bucketconversorsamba";
+	private String sourceKeyName  = "videoteste2.m4v";
+	private String destKeyName    = "videoteste2.mp4";	
+	private String uploadFileName = "C:\\Users\\ferre\\Downloads\\testvideo.m4v";
+	
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+//		System.out.println(java.util.Arrays.asList(request.getParameterNames()));
+//		Enumeration<String> params = request.getParameterNames();
+//		while(params.hasMoreElements())
+//			System.out.println(params.nextElement());
+//		String uploadFile = request.getParameter("file");
+//		System.out.println(request.getParameter("nome"));
+//		System.out.println("Resultado Arquivo:");
+//		System.out.println(uploadFile);
 
-	protected void service(HttpServletRequest request, HttpServletResponse response)
-			throws ServletException, IOException {
-
-		String uploadFile = request.getParameter("file");
-
+		URL signed = null;
+		
+		
 		PrintWriter out = response.getWriter();
 		AmazonS3 s3 = new AmazonS3Client(new ProfileCredentialsProvider());
+		
+        try {
+            System.out.println("Uploading a new object to S3 from a file\n");
+            File file = new File(uploadFileName);
+//            System.out.println("teste");
+            s3.putObject(new PutObjectRequest(bucketName, sourceKeyName, file));
+            
+            java.util.Date expiration = new java.util.Date();
+            long msec = expiration.getTime();
+            msec += 1000 * 60 * 60; // 1 hour.
+            expiration.setTime(msec);
+         
+            GeneratePresignedUrlRequest generatePresignedUrlRequest = 
+                    new GeneratePresignedUrlRequest(bucketName, destKeyName);
+            generatePresignedUrlRequest.setMethod(HttpMethod.GET); // Default.
+            generatePresignedUrlRequest.setExpiration(expiration);
+                   
+            signed = s3.generatePresignedUrl(generatePresignedUrlRequest);
+            
+            String get = "s3://" + bucketName + "/" + sourceKeyName,
+            	   put = "s3://" + bucketName + "/" + destKeyName;
+            
+            zencoderPost(get, put);
 
-		try {
-			System.out.println("Uploading a new object to S3 from a file\n");
-			File file = new File(uploadFileName);
-			System.out.println("teste");
-			PutObjectResult reqResult = s3.putObject(new PutObjectRequest(bucketName,keyName, file));
-			System.out.println(reqResult.getETag());
-			
-
-		} catch (AmazonServiceException ase) {
-			System.out.println("Caught an AmazonServiceException, which " + "means your request made it "
-					+ "to Amazon S3, but was rejected with an error response" + " for some reason.");
-			System.out.println("Error Message:    " + ase.getMessage());
-			System.out.println("HTTP Status Code: " + ase.getStatusCode());
-			System.out.println("AWS Error Code:   " + ase.getErrorCode());
-			System.out.println("Error Type:       " + ase.getErrorType());
-			System.out.println("Request ID:       " + ase.getRequestId());
-		} catch (AmazonClientException ace) {
-			System.out.println("Caught an AmazonClientException, which " + "means the client encountered "
-					+ "an internal error while trying to " + "communicate with S3, "
-					+ "such as not being able to access the network.");
-			System.out.println("Error Message: " + ace.getMessage());
-		}
-
+         } catch (AmazonServiceException ase) {
+            System.out.println("Caught an AmazonServiceException, which " +
+            		"means your request made it " +
+                    "to Amazon S3, but was rejected with an error response" +
+                    " for some reason.");
+            System.out.println("Error Message:    " + ase.getMessage());
+            System.out.println("HTTP Status Code: " + ase.getStatusCode());
+            System.out.println("AWS Error Code:   " + ase.getErrorCode());
+            System.out.println("Error Type:       " + ase.getErrorType());
+            System.out.println("Request ID:       " + ase.getRequestId());
+        } catch (AmazonClientException ace) {
+            System.out.println("Caught an AmazonClientException, which " +
+            		"means the client encountered " +
+                    "an internal error while trying to " +
+                    "communicate with S3, " +
+                    "such as not being able to access the network.");
+            System.out.println("Error Message: " + ace.getMessage());
+        }
+        
+        
+		
 		out.println("<html>");
 		out.println("<body>");
 		out.println("Request realizado com sucesso5");
+		
+		out.println("<iframe src=\"" + signed.toString() + "\" style=\"border:none\"></iframe>");
 		out.println("</body>");
 		out.println("</html>");
+		
+		
+		
+	}
+	
+	public void checkProgress(String id) throws ClientProtocolException, IOException {
+		HttpClient httpclient = HttpClients.createDefault();
+		HttpGet httpput = new HttpGet("https://app.zencoder.com/api/v2/jobs/" + id +  "/progress");
+		httpput.setHeader("Zencoder-Api-Key", "8c7050a93023870d3a3419056c6e00bc");
+	    httpput.setHeader("Accept", "application/json");
+	    httpput.setHeader("Content-type", "application/json");
+	    
+
+//	    GET /api/v2/jobs/465971066/progress HTTP/1.1
+//	    Accept: application/json
+//	    Content-Type: application/json
+//	    Zencoder-Api-Key: 8c7050a93023870d3a3419056c6e00bc
+	    
+		HttpResponse response = httpclient.execute(httpput);
+		HttpEntity aux = response.getEntity();
+	    
+		String responseString = EntityUtils.toString(aux, "UTF-8");
+		System.out.println(responseString);
+		
+		JSONObject jsonObj = new JSONObject(responseString);
+		String state = jsonObj.get("state").toString();
+		System.out.println(state);
+	}
+	
+	
+	public void zencoderPost(String getURL, String putURL) throws ClientProtocolException, IOException {
+		HttpClient httpclient = HttpClients.createDefault();
+		HttpPost httppost = new HttpPost("https://app.zencoder.com/api/v2/jobs");
+		
+		String json = "{ "
+				+ "\"input\": \""+ getURL +"\","
+				+ "\"outputs\": [ { "
+					+ "\"label\": \"mp4 high\","
+					+ "\"url\": \""+ putURL +"\","
+					+ "\"credentials\": \"s3\","
+					+ "\"h264_profile\": \"high\" }"
+				+ " ] } ";
+		
+	    StringEntity entity = new StringEntity(json);
+	    httppost.setEntity(entity);
+	    httppost.setHeader("Accept", "application/json");
+	    httppost.setHeader("Content-type", "application/json");
+	    httppost.setHeader("Zencoder-Api-Key", "8c7050a93023870d3a3419056c6e00bc");
+		
+	    //CloseableHttpResponse response = client.execute(httpPost);
+	    //assertThat(response.getStatusLine().getStatusCode(), equalTo(200));
+	    //client.close();	    
+
+		//Execute and get the response.
+		HttpResponse response = httpclient.execute(httppost);
+		HttpEntity aux = response.getEntity();
+		String responseString = EntityUtils.toString(aux, "UTF-8");
+		
+		JSONObject jsonObj = new JSONObject(responseString);
+		String id = jsonObj.get("id").toString();
+		System.out.println(id);
+		/*HttpEntity entity = response.getEntity();*/
+		checkProgress(id);		
+		
 
 	}
+	
+	
 
 }
